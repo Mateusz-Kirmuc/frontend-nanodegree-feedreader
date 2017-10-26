11bcb5f0ec86700276fa5896df210051b6696ac6
mateusz <mateusz@kirmuc.pl>
2017-10-26 21:17:43 +0200

    Add Initial Entries test suit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,13 +83,26 @@ $(function() {
     })
   });
   /* TODO: Write a new test suite named "Initial Entries" */
+  describe("Initial Entries", function() {
+    beforeEach(function(done) {
+      loadFeed(0, function() {
+        done();
+      });
+    });
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+    it("are present after loadFeed call", function(done) {
+      let entries = $(".feed .entry");
+      expect(entries.length).toBeGreaterThan(0);
+      done();
+    });
+  });
 
-  /* TODO: Write a test that ensures when the loadFeed
-   * function is called and completes its work, there is at least
-   * a single .entry element within the .feed container.
-   * Remember, loadFeed() is asynchronous so this test will require
-   * the use of Jasmine's beforeEach and asynchronous done() function.
-   */
+
 
   /* TODO: Write a new test suite named "New Feed Selection" */
 
